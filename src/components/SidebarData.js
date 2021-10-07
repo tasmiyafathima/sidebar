763d40f4252aad9f2b454c6e538f3e73912b0c11
tasmiyafathima <you@example.com>
2021-10-07 21:36:06 +0530

    Sidebar

---
+++ src/components/SidebarData.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+
+
+export const SidebarData = [
+    {
+        title:"Home",
+     
+        link:"/home"
+    },
+    {
+        title:"Create Ad",
+       
+        link:"/Create Ad"
+    },
+    {
+        title:"Clients",
+       
+        link:"/Clients"
+    },
+    {
+        title:"Ad Analytics",
+        
+        link:"/Ad Analytics"
+    },
+    {
+        title:"Settings",
+       
+        link:"/Settings"
+    }
+]
